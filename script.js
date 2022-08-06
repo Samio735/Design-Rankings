085c226959353fe09efeb436c2856c492b3d1665
Samy <s_rahim@estin.dz>
2022-08-06 11:58:53 +0100

    added play button

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("hello")
+
+const play = document.querySelector('.play')
+const body = document.querySelector('body')
+const audio = document.querySelector('audio')
+const video = document.querySelector('video')
+
+play.addEventListener('click',start)
+
+function start() {
+    body.setAttribute('class','inGame')
+    audio.setAttribute('autoplay','true')
+    audio.load()
+}
